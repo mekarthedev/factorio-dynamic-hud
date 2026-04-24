c8ecd1a05424b79ad8d630f569871f3464a62d5a
aglaron <aglaron.m@gmail.com>
2026-04-24 17:39:16 +0200

    warn about uncommited changes before building

--- build.sh.js
+++ build.sh.js
@@ -3,6 +3,11 @@
 import { $ } from "bun"
 import info from "./info.json"
 
+const modifiedFiles = (await $`git status --porcelain --untracked-files=no`.text()).trim()
+if (modifiedFiles) {
+  throw new Error("Uncommitted changes found; stash or discard before archiving.")
+}
+
 const commitShort = (await $`git rev-parse --short HEAD`.text()).trim()
 const zipName = `${info.name}_${info.version}`
 const folderName = `${zipName}+${commitShort}`
